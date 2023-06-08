900f8022df97b2422f32cf23c8e647a408dcfb4a
Your Name <you@youraddress.com>
2023-06-08 17:34:36 +0500

    feat: completed footer (2023-06-08, 17:34)

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,38 +17,22 @@ export default {
       center: true,
       padding: '1.2rem',
       screens: {
+        'xs': '430px',
         'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-
         'md': '768px',
-        // => @media (min-width: 768px) { ... }
-
         'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-
         'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-
         '2xl': '1460px',
-        // => @media (min-width: 1536px) { ... }
       }
     },
     theme: {
       screens: {
+        'xs': '430px',
         'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-
         'md': '768px',
-        // => @media (min-width: 768px) { ... }
-
         'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-
         'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-
         '2xl': '1460px',
-        // => @media (min-width: 1536px) { ... }
       }
     }
   },
