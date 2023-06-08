fd0cf478a2c49ab36e3ae937a8ac72b37002c274
Your Name <you@youraddress.com>
2023-06-08 18:11:30 +0500

    feat: completed materials section (2023-06-08, 18:11)

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -2,15 +2,17 @@
     <div class="home-page">
         <HeroSection />
         <ServicesSection />
+        <MainMaterialsSection />
     </div>
 </template>
 
 <script>
 
 import HeroSection from './sections/HeroSection.vue'
+import MainMaterialsSection from './sections/MainMaterialsSection.vue';
 import ServicesSection from './sections/ServicesSection.vue';
 
 export default {
-    components: { HeroSection, ServicesSection }
+    components: { HeroSection, ServicesSection, MainMaterialsSection }
 }
 </script>
